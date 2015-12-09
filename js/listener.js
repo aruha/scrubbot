c4a10d5e065a3c2dcb3ae1eaa8ec63c2952e3122
arufa <bakabrigadier@gmail.com>
2015-12-09 01:01:56 -0800

    Implemented basic command recognition and handling

--- js/listener.js
+++ js/listener.js
@@ -1,33 +1,35 @@
 var Discord = require("discord.js");
-var config = require("../json/config.json");
-//var commands = require("../json/commands.json");
-var scripts = require("./initscripts.js")();
-
 var bot = new Discord.Client();
 
-handler = function(commandEntry, message) {
+var config = require("../json/config.json");
+var scripts = require("./initscripts.js")(bot);
 
+
+handler = function(command, message) {
+    var commandEntry = null;
+    for (var i = 0; i < cList.length; i++) {
+        if (cList[i].cmd == command) {
+            commandEntry = cList[i];
+            break;
+        }
+    }
+    if (commandEntry == null) {
+        console.log("invalid...");
+        return;
+    }
+    atlas[commandEntry.file][commandEntry.fn](message);
 }
 
 bot.on("message", function(message){
     var words = message.content.split(" ");
     var permissions = message.channel.permissionsOf(bot.user);
     
-    /*
-    if (!permissions.hasPermission("sendMessages") || words[0][0] != "!" || (words[0][0] == "!" && words[0][1] == "!")) return; //definitely revisit this soon
-    
-    if (commands.commandList.indexOf(words[0]) > -1) //found in commandlist
-    {
-        bot.reply(message, "hello world!");
-    } 
-    else if (words[0].length > config.fragmentThreshold) //no match + conditionals
-    {
-        bot.sendMessage(message.channel, "Unrecognized command. Type ``!help`` for a list of available commands and syntax.");
+    if (words[0].match(/!([A-z])\w/g) != null && permissions.hasPermission("sendMessages")) {
+        handler(words[0], message);
     }
-    */
 });
 
-atlas = scripts.init();
+atlas = scripts.init(bot);
 cList = scripts.formranks(atlas);
 
 bot.login(config.credentials.email, config.credentials.password);
