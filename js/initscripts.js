e1d8acd93e032184d2f183e75f366a197f47aa0d
arufa <bakabrigadier@gmail.com>
2015-12-09 23:39:03 -0800

    Rename formranks -> loadclist, only load .js files

--- js/initscripts.js
+++ js/initscripts.js
@@ -1,28 +1,37 @@
 var fs = require("fs");
 
-module.exports = function(bot) {
+module.exports = function(bot, sendSync) {
     return {
         init: function() {
             var list = [];
             console.log("initscripts.js->init: initializing scripts");
             var files = fs.readdirSync("./scripts");
             for (var i = 0; i < files.length; i++) {
-                list[files[i]] = require("../scripts/" + files[i])(bot);
-                console.log("initscripts.js->init: registered " + files[i]);
+                if (files[i].match(/(\.js)$/g) !== null) {
+                    list[files[i]] = require("../scripts/" + files[i])(bot, sendSync);
+                    console.log("initscripts.js->init: registered " + files[i]);
+                }
             }
             console.log("initscripts.js->init: completed script initialization with " + Object.keys(list).length + " file(s)");
             return list;
         },
-        formranks: function(list) {
+        loadclist: function(list) {
             var cmdlist = [], index = 0;
-            console.log("initscripts.js->formranks: initializing command list");
+            console.log("initscripts.js->loadclist: initializing command list");
             for (key_name in list) {
                 for (var j = 0; j < list[key_name].commands.length; j++) {
+                    for (var k = 0; k < cmdlist.length; k++) {
+                        if (list[key_name].commands[j].cmd === cmdlist[k].cmd) {
+                            console.log("fatal error: duplicate command detected (" + cmdlist[k].cmd + " in " + cmdlist[k].file + " and " + key_name + ")");
+                            process.exit(-1);
+                        }
+                    }
                     cmdlist[index++] = list[key_name].commands[j];
-                    console.log("initscripts.js->formranks: registered " + list[key_name].commands[j].cmd);
+                    console.log("initscripts.js->loadclist: registered " + list[key_name].commands[j].cmd 
+                                + " to " + list[key_name].commands[j].fn + " in " + list[key_name].commands[j].file);
                 }
             }
-            console.log("initscripts.js->formranks: finished initializing command list with " + cmdlist.length + " command(s)");
+            console.log("initscripts.js->loadclist: finished initializing command list with " + cmdlist.length + " command(s)");
             return cmdlist;
         }
     };
