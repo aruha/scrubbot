850922ad9649ada1c7eb35a752dc51e62087a6d9
arufa <bakabrigadier@gmail.com>
2015-12-09 23:38:07 -0800

    Changed formatting on dice input

--- scripts/tabletop.js
+++ scripts/tabletop.js
@@ -10,23 +10,24 @@ module.exports = function(bot, sendSync) {
             var words = message.content.split(" "), total = 0;
             
             if (words.length === 2 && words[1] === "?") {
-                bot.sendMessage(message.channel, "Description: Rolls virtual dice and displays output in channel.\nSyntax: !dice [number of dice] [max value of one die] _[bonus]_");
+                bot.sendMessage(message.channel, "Description: Rolls virtual dice and displays output in channel.\nSyntax: !dice ``num of dice``d``max dice value``+``bonus``");
                 return;
-            }            
-            if (words.length < 3) {
+            } else if (words.length > 2) {
                 poorSyntax("!dice", message);
                 return;
             }
-            if (isNaN(parseInt(words[1])) || isNaN(parseInt(words[2]))) {
-                poorSyntax("!dice", message);
-                return;
+            if (words[1].match(/[0-9]+d[0-9]+\+[0-9]+/g)) {
+                var numbers = [], total = 0;
+                for (var i = 0; i < 3; i++) {
+                    numbers[i] = parseInt(words[1].match(/[0-9]+/g)[i]);
+                }
+                for (var j = 0; j < numbers[0]; j++) 
+                    total += Math.ceil(Math.random() * numbers[1]);
+                total += numbers[2];
+                bot.sendMessage(message.channel, "You rolled a " + Math.ceil(total));
+            } else {
+                bot.sendMessage(message.channel, "Invalid dice given. Input is formatted ``num of dice``d``max dice value``+``bonus``");
             }
-            
-            for (var i = 0; i < parseInt(words[1]); i++) {
-                total += Math.random() * parseInt(words[2]);
-            }
-            if (!isNaN(parseInt(words[3]))) total += parseInt(words[3]);
-            bot.sendMessage(message.channel, "You rolled a " + Math.ceil(total));
             return;
         }
     };
