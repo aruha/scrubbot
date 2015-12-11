417b7073ee7473d5d0a46a01bedfd9bdbc5beac1
arufa <bakabrigadier@gmail.com>
2015-12-10 23:36:07 -0800

    Changed to support names with spaces

--- scripts/inspector.js
+++ scripts/inspector.js
@@ -7,9 +7,13 @@ module.exports = function(bot, sendSync) {
     return {
         commands: commands,
         inspect: function (message) {
-            var words = message.content.split(" "), user = message.channel.server.members.get("username", words[1]);
+            //extract username
+            var words = message.content.split(" ");
+            words.splice(0, 1);
+            words = words.join(" ");
+            var user = message.channel.server.members.get("username", words)
             bot.sendMessage(message.author, "Name: " + user.username + "\nID: " + user.id);
-            bot.sendMessage(message.channel, "_Inspect info for " + words[1] + " sent._");
+            bot.sendMessage(message.channel, "_Inspect info for " + words + " sent._");
             return;
         }
     };
