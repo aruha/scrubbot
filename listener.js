5e0541a791eaa1f8aa6b255fa129832478abf9b0
arufa <bakabrigadier@gmail.com>
2015-12-12 18:02:05 -0800

    Added message cooldowns

--- listener.js
+++ listener.js
@@ -48,12 +48,13 @@ handler = function(command, message) {
 }
 
 bot.on("message", function(message) {
+    var words = message.content.split(" ");
+    
     if (message.channel.recipient) { //ignore pms
         console.log("received pm");
         return;
     }
 
-    var words = message.content.split(" ");
     var permissions = message.channel.permissionsOf(bot.user);
     if (sendSync) {
         handler(sendSync.cmd, sendSync.msg);
@@ -67,14 +68,31 @@ bot.on("message", function(message) {
             }
         }
         bot.sendMessage(message.channel, "Available commands: " + out);
-    } else if (words[0].match(/!{1,2}\w+/g) !== null && permissions.hasPermission("sendMessages")) {
-        console.log("event: registered call to " + words[0] + " command");
+    } else if (words[0].match(/^!{1,2}\w+/g) !== null && permissions.hasPermission("sendMessages")) {
+        console.log("event: registered call to " + words[0] + " command by " + message.author.username);
+        var cooldown = config.userCDInSeconds;
+        /*for (var i = 0; i < bot.admins.length; i++) {
+            if (bot.admins[i] == message.author.id) {
+                cooldown = config.adminCDInSeconds;
+                break;
+            }
+        }*/
+        if (bot.admins.indexOf(message.author.id) > -1) {
+            cooldown = config.adminCDInSeconds;
+        }
+        if (Date.now() < bot.cooldowns[message.author.id] + cooldown * 1000) {
+            console.log("...but their cooldown was still active");
+            bot.reply(message, "your cooldown is still active for " + ((bot.cooldowns[message.author.id] + cooldown * 1000 - Date.now())/ 1000) + " seconds");
+            return;
+        }
+        bot.cooldowns[message.author.id] = Date.now();
         handler(words[0], message);
     }
 });
 
 atlas = scripts.init(bot);
 bot.admins = scripts.loadAdmins(config);
+bot.cooldowns = {};
 cList = scripts.loadCList(atlas);
 
 bot.login(config.credentials.email, config.credentials.password);
