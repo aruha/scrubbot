3ee7e05c6555667c1e39bf26667a039bc579e537
arufa <bakabrigadier@gmail.com>
2016-01-17 19:29:09 -0800

    Simplified command loading, added file sending

--- scripts/simple.js
+++ scripts/simple.js
@@ -1,32 +1,25 @@
 ﻿//This script automatically loads random single-message (responseList) commands, and multiple-message (sequentialList)
 //from ./storage/simple.js/commands.json -- to add more commands, simply edit that file using the proper format.
 
-var fileName = __filename.slice(__dirname.length + 1);
-var storagePath = "./storage/" + fileName + "/";
-
-var commandJson = require(storagePath + "responses.json"),
+var fileName = __filename.slice(__dirname.length + 1),
+    commandJson = require("./storage/" + fileName + "/responses.json"),
     common = require("../lib/common.js"),
     responseList = commandJson.responseList,
     sequentialList = commandJson.sequentialList,
+    imageList = commandJson.imageList,
     common = require("../lib/common.js");
 
 var commands = [];
 
 function initSimple(cmdList) {
-    var newCmd;
-    for (var resName in responseList) {
-        newCmd = {};
-        newCmd.cmd = resName;
-        newCmd.fn = "simple";
-        newCmd.file = fileName;
-        cmdList[commands.length] = newCmd;
-    }
-    for (var seqName in sequentialList) {
-        newCmd = {};
-        newCmd.cmd = seqName;
-        newCmd.fn = "synchsend";
-        newCmd.file = fileName;
-        cmdList[commands.length] = newCmd;
+    for (var listName in commandJson) {
+        for (var cmdName in commandJson[listName]) {
+            var newCmd = {};
+            newCmd.cmd = cmdName;
+            newCmd.fn = listName;
+            newCmd.file = fileName;
+            cmdList.push(newCmd);
+        }
     }
     return cmdList;
 }
@@ -36,26 +29,34 @@ module.exports = function(bot) {
         commands: initSimple(commands),
         simple: function(message) {
             var words = message.content.split(" "),
-                picked = Math.floor(Math.random() * responseList[words[0]].length);
-            bot.sendMessage(message.channel, responseList[words[0]][picked]);
+                possibleResults = commandJson.simple[words[0]],
+                picked = Math.floor(Math.random() * possibleResults.length);
+            bot.sendMessage(message.channel, possibleResults[picked]);
             return;
         },
         synchsend: function(message) {
-            var words = message.content.split(" ");
+            var words = message.content.split(" "),
+                messageList = commandJson.synchsend[words[0]];
             if (!bot.sendSync || (bot.sendSync && bot.sendSync.cmd !== words[0])) {
                 bot.sendSync = new common.Session(message, words[0]);
                 console.log(words[0] + ": opening sync thread");
-                bot.sendMessage(message.channel, sequentialList[words[0]][++bot.sendSync.seqn]);
+                bot.sendMessage(message.channel, messageList[++bot.sendSync.seqn]);
             } else {
                 console.log(words[0] + ": stepping in sync thread (seqn: " + bot.sendSync.seqn + " => " + (bot.sendSync.seqn + 1) + ")");
-                if (sequentialList[words[0]][++bot.sendSync.seqn] !== undefined) {
-                    bot.sendMessage(message.channel, sequentialList[words[0]][bot.sendSync.seqn]);
+                if (messageList[++bot.sendSync.seqn] !== undefined) {
+                    bot.sendMessage(message.channel, messageList[bot.sendSync.seqn]);
                 } else {
                     console.log(words[0] + ": ending sync thread");
                     bot.sendSync = undefined;
                 }
             }
             return bot.sendSync;
+        },
+        imagesend: function (message) {
+            var words = message.content.split(" "),
+                possibleResults = commandJson.imagesend[words[0]],
+                picked = Math.floor(Math.random() * possibleResults.length);
+            bot.sendFile(message.channel, "./scripts/storage/" + fileName + "/" + possibleResults[picked], possibleResults[picked]);
         },
         getout: function(message) {
             var words = message.content.split(" ");
